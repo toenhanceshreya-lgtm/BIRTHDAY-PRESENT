93f05cab13882d308ef8d331e1df936d911d4096
AbhinavRai040604 <162104255+AbhinavRai040604@users.noreply.github.com>
2026-07-14 00:12:48 +0530

    fix build

---
+++ .output/server/_tanstack-start-manifest_v-D-F9SA7q.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-D-F9SA7q.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/Asus/Desktop/sar/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-CKzGqA2M.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CKzGqA2M.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/Asus/Desktop/sar/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-BCw8gxYG.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
